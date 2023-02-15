6ec15a06c803888539b97f4808a135ccdccef677
ryan10-wq <116791572+ryan10-wq@users.noreply.github.com>
2023-02-15 10:36:01 +0300

    Add files via upload

---
+++ Class work.py
@@ -0,0 +1,18 @@
+Fname=input("enter your first name")
+Lname=input("enter your last name")
+Mname=input("enter your middle name")
+age=int(input(("please enter your age ")))
+address=input("enter your address")
+city=input("enter your city")
+
+#output
+
+
+
+print("*********")
+
+
+
+print(Fname+ Lname)
+print(age)
+print(city)
